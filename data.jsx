1f8f8261fdbea4edd364f817775652df1451855d
eduarf <fraude_f@yahoo.com>
2023-08-29 13:22:44 +0300

    how-to-hire section markup and styling

--- data.jsx
+++ data.jsx
@@ -1,12 +1,14 @@
 import { nanoid } from "nanoid";
 import { HiOutlineUserGroup } from "react-icons/hi";
-import { BsCalendar2Check, BsFillBagFill } from "react-icons/bs";
+import { RiShieldStarLine } from "react-icons/ri";
+import { BsCalendar2Check, BsFillBagFill, BsPinAngle } from "react-icons/bs";
+import { FaPenSquare } from "react-icons/fa";
 import FindTalentDropdown from "./src/components/find-talent-dropdown/FindTalentDropdown";
 import FindWorkDropdown from "./src/components/find-work-dropdown/FindWorkDropdown";
 import WhyUpworkDropdown from "./src/components/why-upwork-dropdown/WhyUpworkDropdown";
-import microsoftSvg from './src/assets/microsoft.svg';
-import airbnbSvg from './src/assets/airbnb.svg';
-import bissellSvg from './src/assets/bissell.svg';
+import microsoftSvg from "./src/assets/microsoft.svg";
+import airbnbSvg from "./src/assets/airbnb.svg";
+import bissellSvg from "./src/assets/bissell.svg";
 
 export const navItems = [
   {
@@ -131,156 +133,167 @@ export const whyUpworkDropdownItemsLeft = [
   },
   {
     id: nanoid(),
-    header: 'How to hire',
-    description: 'Learn about the different ways to get work done.',
+    header: "How to hire",
+    description: "Learn about the different ways to get work done.",
   },
   {
     id: nanoid(),
-    header: 'How to find work',
-    description: 'Learn about how to grow your independent career.'
+    header: "How to find work",
+    description: "Learn about how to grow your independent career.",
   },
 ];
 
 export const whyUpworkDropdownItemsRight = [
   {
     id: nanoid(),
-    header: 'Guides',
-    description: 'Getting Started as a Freelancer',
+    header: "Guides",
+    description: "Getting Started as a Freelancer",
   },
   {
     id: nanoid(),
-    header: 'Guides',
-    description: 'Growing Your Freelance Career',
+    header: "Guides",
+    description: "Growing Your Freelance Career",
   },
   {
     id: nanoid(),
-    header: 'Guides',
-    description: 'Hiring & Working with Independent Talent',
+    header: "Guides",
+    description: "Hiring & Working with Independent Talent",
   },
 ];
 
 export const findTalentDropdownItemsLeft = [
   {
     id: nanoid(),
-    header: 'Post a job and hire a pro',
-    subtitle: 'Talent Marketplace',
-    identity: 'TALENT_MARKETPLACE'
+    header: "Post a job and hire a pro",
+    subtitle: "Talent Marketplace",
+    identity: "TALENT_MARKETPLACE",
   },
   {
     id: nanoid(),
-    header: 'Browse and buy projects',
-    subtitle: 'Project catalog',
-    identity: 'PROJECT_CATALOG'
+    header: "Browse and buy projects",
+    subtitle: "Project catalog",
+    identity: "PROJECT_CATALOG",
   },
   {
     id: nanoid(),
-    header: 'Get advice from an industry expert',
-    subtitle: 'Consultations',
-    identity: 'CONSULTATIONS'
+    header: "Get advice from an industry expert",
+    subtitle: "Consultations",
+    identity: "CONSULTATIONS",
   },
 ];
 
 export const talentMarketplaceItems = [
   {
     id: nanoid(),
-    header: 'Development & IT',
+    header: "Development & IT",
   },
   {
     id: nanoid(),
-    header: 'Design & Creative',
+    header: "Design & Creative",
   },
   {
     id: nanoid(),
-    header: 'Sales & Marketing',
+    header: "Sales & Marketing",
   },
   {
     id: nanoid(),
-    header: 'Writing & Translation',
+    header: "Writing & Translation",
   },
   {
     id: nanoid(),
-    header: 'Admin & Customer Support',
+    header: "Admin & Customer Support",
   },
   {
     id: nanoid(),
-    header: 'Finance & Accounting',
+    header: "Finance & Accounting",
   },
   {
     id: nanoid(),
-    header: 'HR & Training',
+    header: "HR & Training",
   },
   {
     id: nanoid(),
-    header: 'Legal',
+    header: "Legal",
   },
   {
     id: nanoid(),
-    header: 'Engineering & Architecture',
+    header: "Engineering & Architecture",
   },
   {
     id: nanoid(),
-    header: 'Hire freelancers',
+    header: "Hire freelancers",
   },
 ];
 
 export const projectCatalogItems = [
   {
     id: nanoid(),
-    header: 'Logo Design',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/logo.cd04b531.jpg'
+    header: "Logo Design",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/logo.cd04b531.jpg",
   },
   {
     id: nanoid(),
-    header: 'Articles & Blog Posts',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/blog.d73af7fb.jpg'
+    header: "Articles & Blog Posts",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/blog.d73af7fb.jpg",
   },
   {
     id: nanoid(),
-    header: 'WordPress',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/wp.1acec44b.jpg',
+    header: "WordPress",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/wp.1acec44b.jpg",
   },
   {
     id: nanoid(),
-    header: 'Social Media Marketing',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/smm.1a3a67a0.jpg'
+    header: "Social Media Marketing",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/smm.1a3a67a0.jpg",
   },
   {
     id: nanoid(),
-    header: 'Video Editing',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/video.8e2caeb1.jpg',
+    header: "Video Editing",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/video.8e2caeb1.jpg",
   },
   {
     id: nanoid(),
-    header: 'SEO',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/seo.bb0c74c3.jpg',
+    header: "SEO",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/seo.bb0c74c3.jpg",
   },
 ];
 
 export const consultationsItems = [
   {
     id: nanoid(),
-    header: 'Web Programming',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/web-programming.bba7813a.png'
+    header: "Web Programming",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/web-programming.bba7813a.png",
   },
   {
     id: nanoid(),
-    header: 'AI & Machine Learning',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/ai.b8aa0451.png'
+    header: "AI & Machine Learning",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/ai.b8aa0451.png",
   },
   {
     id: nanoid(),
-    header: 'Marketing & Strategy',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/marketing.43b52b75.png'
+    header: "Marketing & Strategy",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/marketing.43b52b75.png",
   },
   {
     id: nanoid(),
-    header: 'Social Media Marketing',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/social-media.775752fd.png'
+    header: "Social Media Marketing",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/social-media.775752fd.png",
   },
   {
     id: nanoid(),
-    header: 'Website Builders & CMS',
-    image: 'https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/website-builders.6acb130a.png'
+    header: "Website Builders & CMS",
+    image:
+      "https://www.upwork.com/static/assets/TopNavSsi/visitor-v2/img/website-builders.6acb130a.png",
   },
 ];
 
@@ -296,5 +309,29 @@ export const trustedByItems = [
   {
     id: nanoid(),
     svg: bissellSvg,
-  }
+  },
+];
+
+export const howToHireItems = [
+  {
+    id: nanoid(),
+    header: "No cost to join",
+    description:
+      "Register and browse professionals, explore projects, or even book a consultation.",
+    icon: <FaPenSquare />,
+  },
+  {
+    id: nanoid(),
+    header: "Post a job and hire top talent",
+    description:
+      "Finding talent doesn’t have to be a chore. Post a job or we can search for you!",
+    icon: <BsPinAngle />,
+  },
+  {
+    id: nanoid(),
+    header: "Work with the best—without breaking the bank",
+    description:
+      "Upwork makes it affordable to up your work and take advantage of low transaction rates.",
+    icon: <RiShieldStarLine />,
+  },
 ];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/header/Header";
+import HowToHire from "../../components/how-to-hire/HowToHire";
 import "./Home.scss";
 
 const Home = () => {
   return <div>
     <Header />
+    <HowToHire />
   </div>;
 };
 
