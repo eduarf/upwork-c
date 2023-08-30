22d00a1cdcc25db1d1199c2e455addbe0e7be1a6
eduarf <fraude_f@yahoo.com>
2023-08-30 17:26:11 +0300

    category pills section show and less button

--- src/components/category-pills/CategoryPills.jsx
+++ src/components/category-pills/CategoryPills.jsx
@@ -1,8 +1,30 @@
-import "./CategoryPills.scss";
+import { useState, useEffect } from "react";
+import { FaStar } from "react-icons/fa";
 import { categoryPillsItems } from "../../../data";
-import { FaStar } from 'react-icons/fa';
+import "./CategoryPills.scss";
 
 const CategoryPills = () => {
+  const [isScreenWidth680, setIsScreenWidth680] = useState(
+    window.innerWidth < 680
+  );
+  const [showMore, setShowMore] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 680) {
+        setIsScreenWidth680(true);
+      } else {
+        setIsScreenWidth680(false);
+      }
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const lastThreeItems = categoryPillsItems.slice(-3);
+
   return (
     <div className="category-pills">
       <div className="category-pills__top-side">
@@ -13,20 +35,33 @@ const CategoryPills = () => {
         </div>
       </div>
       <div className="category-pills__bottom-side">
-        {categoryPillsItems.map((item) => {
-            return <div key={item.id} className="category-card">
-                <h1>{item.header}</h1>
-                <div className="star-skills">
-                    <div className="star">
-                        <FaStar className="icon" />
-                        <span>{item.star}</span>
-                    </div>
-                    <div className="skills">
-                        <span>{item.skills} skills</span>
-                    </div>
-                </div>
+        {categoryPillsItems.map((item) => (
+          <div
+            key={item.id}
+            className={`category-card ${
+              isScreenWidth680 && lastThreeItems.includes(item) && !showMore
+                ? "category-card-hidden"
+                : ""
+            }`}
+          >
+            <h1>{item.header}</h1>
+            <div className="star-skills">
+              <div className="star">
+                <FaStar className="icon" />
+                <span>{item.star}</span>
+              </div>
+              <div className="skills">
+                <span>{item.skills} skills</span>
+              </div>
             </div>
-        })}
+          </div>
+        ))}
+        {isScreenWidth680 && !showMore && (
+          <span className="show-less" onClick={() => setShowMore(true)}>Show more (3)</span>
+        )}
+        {isScreenWidth680 && showMore && (
+          <span className="show-less" onClick={() => setShowMore(false)}>Show less</span>
+        )}
       </div>
     </div>
   );
