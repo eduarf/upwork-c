46a34c743330865166782a8639ea6dfa904c4260
eduarf <fraude_f@yahoo.com>
2023-08-31 11:11:21 +0300

    ForEnterPrise styling

--- src/components/for-enterprise/ForEnterPrise.jsx
+++ src/components/for-enterprise/ForEnterPrise.jsx
@@ -1,5 +1,7 @@
 import "./ForEnterPrise.scss";
 import { enterpriseListItems } from "../../../data";
+import enterpriseImage from '../../assets/enterprise-image.jpg';
+import CustomBtn from '../buttons/customize-btns/CustomBtn';
 
 const ForEnterPrise = () => {
   return (
@@ -7,7 +9,7 @@ const ForEnterPrise = () => {
       <div className="enterprise__left">
         <h1 className="enterprise__left--header">Enterprise Suite</h1>
         <h1 className="enterprise__left--header-big">
-          This is how good companies find good company.
+          This is how <br /> <span>good companies <br /> find good company.</span>
         </h1>
         <p className="enterprise__left--para">
           Access the top 1% of talent on Upwork, and a full suite of hybrid
@@ -25,8 +27,11 @@ const ForEnterPrise = () => {
             })}
           </ul>
         </div>
+        <CustomBtn name='Learn more'  type='primary primary-white' />
       </div>
-      <div className="enterprise__right"></div>
+      <div className="enterprise__right">
+        <img src={enterpriseImage} alt="image" />
+      </div>
     </div>
   );
 };
