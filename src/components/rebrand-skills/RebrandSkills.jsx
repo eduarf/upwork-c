99c0bc1ee86943d42ffae5b534efff3d486cabbc
eduarf <fraude_f@yahoo.com>
2023-09-08 23:35:39 +0300

    rebrandSlice

--- src/components/rebrand-skills/RebrandSkills.jsx
+++ src/components/rebrand-skills/RebrandSkills.jsx
@@ -1,11 +1,34 @@
-import './RebrandSkills.scss';
+import { useSelector } from "react-redux";
+import "./RebrandSkills.scss";
+import { nanoid } from "nanoid";
 
 const RebrandSkills = () => {
+  const rebrandItems = useSelector((state) => state.rebrandItems.rebrandItems);
+  const visibleItems = rebrandItems.filter((item) => item.visibility === true);
   return (
-    <div>
-      Rebrand Skills
-    </div>
-  )
-}
+    <section className="rebrand-skills">
+      <div className="rebrand-skills__left">
+        <ul>
+          {rebrandItems.map((item) => {
+            return <li key={item.id}>{item.category}</li>;
+          })}
+        </ul>
+      </div>
+      <div className="rebrand-skills__right">
+        <ul>
+          {visibleItems.map((item) => (
+            <li key={item.id}>
+              <ul>
+                {item.names.map((name) => (
+                  <li key={nanoid()} className="rebrand-skills__right--item">{name}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default RebrandSkills;
--- store.js
+++ store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import hamburgerReducer from './src/features/hamburger/hamburgerSlice';
 import searchReducer from './src/features/search/searchSlice';
+import rebrandITemsReducer from './src/features/rebrandItems/rebrandSlice';
 
 export const store = configureStore({
     reducer: {
         hamburger: hamburgerReducer,
         search: searchReducer,
+        rebrandItems: rebrandITemsReducer,
     },
 });
